e84110e7c2173e0991a245f78662b651ee60267f
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-07-31 16:29:46 -0400

    compelte lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibb_list = []
+    for i in range(length):
+        if i == 0 or i == 1:
+            fibb_list.append(i)
+        else:
+            fibb_list.append(fibb_list[i-2] + fibb_list[i-1])
+    print(fibb_list)
